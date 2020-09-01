1f35b62d418186fca913b87c8ed85241a3e8d0a0
papyrinIS <papyrin@sumirea.ru>
2020-09-01 22:16:21 +0300

    Updates

---
+++ precache-manifest.67605c5bec92c1072f7f6cf960888f05.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7b2a23403fd963d9965ab64cbc71c0e",
+    "url": "/app-chat/index.html"
+  },
+  {
+    "revision": "1c456183668db95d0a96",
+    "url": "/app-chat/static/css/2.c2bc211b.chunk.css"
+  },
+  {
+    "revision": "69ff8baee74d1d5c6679",
+    "url": "/app-chat/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1c456183668db95d0a96",
+    "url": "/app-chat/static/js/2.4d939dd8.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/app-chat/static/js/2.4d939dd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69ff8baee74d1d5c6679",
+    "url": "/app-chat/static/js/main.dd03ad53.chunk.js"
+  },
+  {
+    "revision": "a355a6245a421f193272",
+    "url": "/app-chat/static/js/runtime-main.1b90ec71.js"
+  },
+  {
+    "revision": "775bc248fcb45179692bc5209e13faeb",
+    "url": "/app-chat/static/media/close.775bc248.png"
+  },
+  {
+    "revision": "2c3e6019a6a08b739bb607a4fc17ed38",
+    "url": "/app-chat/static/media/deleteIcon.2c3e6019.png"
+  },
+  {
+    "revision": "7f7b9c43c1f0920b6f24be6657d90965",
+    "url": "/app-chat/static/media/editIcon.7f7b9c43.png"
+  },
+  {
+    "revision": "12ba72f0e3f84c4669cd8734d90ab395",
+    "url": "/app-chat/static/media/smile.12ba72f0.png"
+  }
+]);
